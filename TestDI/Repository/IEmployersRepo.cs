309a7c427378f5ecae031526e92c87ae869ea364
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-06 16:32:25 +0300

    Added interface of employers repository

---
+++ TestDI/Repository/IEmployersRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestDI.Repository.Models;
+
+namespace TestDI.Repository
+{
+    interface IEmployersRepo
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int employeeID);
+        void Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int employeeID);
+        void Save();
+    }
+}
