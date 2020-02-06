320fb91813fa688614507db9e5827584d1834b57
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-06 17:20:01 +0300

    Add GetEmployee and Save servicies

---
+++ TestDI/BusinessLogic/Services/GetEmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestDI.Repository;
+using TestDI.Repository.Models;
+
+namespace TestDI.BusinessLogic.Services
+{
+    public class GetEmployeeService : IGetEmployeeService
+    {
+        public EmployersRepo employersRepo { get; set; }
+        public GetEmployeeService(EmployersRepo employersRepo)
+        {
+            this.employersRepo = employersRepo;
+        }
+        public Employee GetEmployee(int employeeID)
+        {
+            return employersRepo.GetById(employeeID);
+        }
+    }
+}
---
+++ TestDI/BusinessLogic/Services/SaveService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestDI.Repository;
+
+namespace TestDI.BusinessLogic.Services
+{
+    public class SaveService : ISaveService
+    {
+        public EmployersRepo employersRepo { get; set; }
+        public SaveService(EmployersRepo employersRepo)
+        {
+            this.employersRepo = employersRepo;
+        }
+
+        public void Save()
+        {
+            employersRepo.Save();
+        }
+    }
+}
