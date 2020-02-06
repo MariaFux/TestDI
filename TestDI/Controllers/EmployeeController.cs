54200adecc14394f9be72e4561cabbed17d9b76f
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-06 17:41:19 +0300

    Added save method in employee controller

---
+++ TestDI/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestDI.BusinessLogic.Services;
+
+namespace TestDI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public SaveService saveService { get; set; }
+        public GetEmployeeService employeeService { get; set; }
+        public EmployeeController(SaveService saveService, GetEmployeeService employeeService)
+        {
+            this.saveService = saveService;
+            this.employeeService = employeeService;
+        }
+
+        public void Save()
+        {
+            saveService.Save();
+        }
+    }
+}
