b039918b516bf9a33d7b9726fd3aa4767d1cc9fc
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-06 16:44:14 +0300

    Added employers DBContext in business logic

---
+++ TestDI/BusinessLogic/Models/EmployersDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestDI.BusinessLogic.Models
+{
+    public class EmployersDBContext : DbContext
+    {
+        public EmployersDBContext()
+        {
+        }
+
+        public EmployersDBContext(DbContextOptions<EmployersDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employers { get; set; }
+    }
+}
